0c0d2c6e9c8925d743a8255231e60c3d86aec728
nuel07 <yehanganep@gmail.com>
2022-05-18 07:12:53 -0700

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/vbin/env python3
+"""Authentication class for the API"""
+
+from flask import Flask, request
+from typing import List, TypeVar
+
+
+class Auth:
+    """an authentication class"""
+    def require_auth(self, path:str, excluded_paths: List[str])-> bool:
+        """returns False"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """returns None"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None"""
+        return None
+
