ed6740ede750f273581ab9abbd5a02d726589507
nuel07 <yehanganep@gmail.com>
2022-05-23 03:42:35 -0700

    pep fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,9 +34,7 @@ def before_request():
         return
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.session_cookie(request) is None:
+    if not auth.authorization_header(request) and not auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
