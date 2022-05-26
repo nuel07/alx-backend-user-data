eb4e72f5dc877d4cbed69338b226cc26bc276744
nuel07 <yehanganep@gmail.com>
2022-05-26 04:12:11 -0700

    task 2 and 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
-from typing import TypeVar
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 class DB:
@@ -32,9 +33,34 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """returns a User object"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        returns the first row found in the users table
+        as filtered by the input arguments
+        """
+        try:
+            new_user = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
+            raise InvalidRequestError
+        if not new_user:
+            raise NoResultFound
+        return new_user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """updates user's attributes"""
+        try:
+            new_user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError
+        for k, v in kwargs.items():
+            if hasattr(new_user, k):
+                setattr(new_user, k, v)
+            raise ValueError
+        self._session.commit()
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -17,3 +17,7 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return "<User(email='%s', hashed_password='%s', session_id='%s', reset_token='%s')>" % (
+            self.email, self.hashed_password, self.session_id, self.reset_token)
