6df0f629d3d9b36a2950e9b8c8ec123cf8357d8f
nuel07 <yehanganep@gmail.com>
2022-05-28 08:21:58 -0700

    pep fix

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,7 +6,8 @@ from flask import abort, request, jsonify
 from models.user import User
 
 
-@app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
+@app_views.route('/auth_session/login/',
+                 methods=['POST'], strict_slashes=False)
 def login():
     """ returns json representation of all users logged in"""
     email = request.form.get("email")
@@ -16,10 +17,10 @@ def login():
     if not password:
         return jsonify({"error": "password missing"}), 400
     my_users = User.search({"email": email})
-    
+
     if not my_users:    
         return jsonify(error="no user found for this email"), 404
-    
+
     for user in my_users:
         if not user.is_valid_password(password):
             return jsonify({"error": "wrong password"}), 401
@@ -28,9 +29,9 @@ def login():
             sesion_id = auth.create_session(user.id)
             sesion_name = os.getenv("SESSION_NAME")
             users = jsonify(user.to_json())
-            users.set_cookie(sesion_name, sesion_id)            
+            users.set_cookie(sesion_name, sesion_id)
             return users
-        
+
 
 @app_views.route('/auth_session/logout/',
                  methods=['DELETE'], strict_slashes=False)
@@ -38,7 +39,6 @@ def logout():
     """ user logout """
     from api.v1.app import auth
     destroy_session = auth.destroy_session(request)
-    
     if destroy_session:
         return jsonify({}), 200
     abort(404)
