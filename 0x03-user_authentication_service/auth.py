5e64f52146ca67b3504aa3921482e578bcae6edd
nuel07 <yehanganep@gmail.com>
2022-05-28 00:57:00 -0700

    task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,5 +52,5 @@ class Auth:
             return "User doesn't exist"
         else:
             session_id = _generate_uuid()
-            self._db.update_user(user_id, session_id=session_id)
+            self._db.update_user(new_user.id, session_id=session_id)
             return session_id
